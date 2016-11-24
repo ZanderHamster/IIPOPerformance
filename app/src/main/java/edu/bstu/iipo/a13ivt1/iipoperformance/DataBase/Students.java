0ea051599281726a2492c18322ea07449469c25c
ZanderHamster <zanderhamster@yandex.ru>
2016-11-24 21:43:51 +0300

    Добавлена БД

---
+++ app/src/main/java/edu/bstu/iipo/a13ivt1/iipoperformance/DataBase/Students.java
@@ -0,0 +1,25 @@
+package edu.bstu.iipo.a13ivt1.iipoperformance.DataBase;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+import edu.bstu.iipo.a13ivt1.iipoperformance.DataBase.DB;
+
+@Table(database = DB.class)
+public class Students extends BaseModel{
+    @Column
+    @PrimaryKey(autoincrement = true)
+    int id;
+
+    @Column
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/edu/bstu/iipo/a13ivt1/iipoperformance/MyApplication.java
@@ -0,0 +1,14 @@
+package edu.bstu.iipo.a13ivt1.iipoperformance;
+
+import android.app.Application;
+
+import com.raizlabs.android.dbflow.config.FlowConfig;
+import com.raizlabs.android.dbflow.config.FlowManager;
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlowManager.init(new FlowConfig.Builder(this).build());
+    }
+}
